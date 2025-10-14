0e99478141b1857939a16f0c7f83149372a226c1
Joshua Choy <joshkchoy@gmail.com>
2025-10-14 11:51:26 -0700

    Fixed sorted.py

--- sorted.py
+++ sorted.py
@@ -1,4 +1,4 @@
 def reverse_sort_dictionary(dictionary):
     for key in dictionary:
         dictionary[key] = dictionary[key][0]
-    return dict(sorted(dictionary.items(), key=lambda item: item[1], reverse=True))
+    return list(sorted(dictionary.items(), key=lambda item: item[1], reverse=True))
