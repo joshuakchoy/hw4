18c584cbe76e55b9d939a14bd112f41904ee10cd
Joshua Choy <joshkchoy@gmail.com>
2025-10-14 19:20:49 -0700

    Finished cacti.py

---
+++ cacti.py
@@ -0,0 +1,22 @@
+def cacti_number(func):
+    def wrapper(plot):
+        rows = len(plot)
+        cols = len(plot[0])
+        counter = 0
+
+        temp = [row[:] for row in plot]
+
+        for i in range(rows):
+            for j in range(cols):
+                if temp[i][j] == 0:
+                    up = i == 0 or temp[i - 1][j] == 0
+                    down = i == rows - 1 or temp[i + 1][j] == 0
+                    left = j == 0 or temp[i][j - 1] == 0
+                    right = j == cols - 1 or temp[i][j + 1] == 0
+
+                    if up and down and left and right:
+                        temp[i][j] = 1
+                        counter += 1
+
+        return counter
+    return wrapper
