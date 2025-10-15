a930afef85b1d65c5e4d0b2641f45b4bae7f0620
Joshua Choy <joshkchoy@gmail.com>
2025-10-14 19:08:03 -0700

    Finished bug1.py

---
+++ bug1.py
@@ -0,0 +1,35 @@
+class Base:
+    def __init__(self, x, y, size):
+        self.x = x
+        self.y = y
+        self.size = size
+
+
+
+class Circle(Base):
+    def __init__(self, x, y, size):
+        super().__init__(x, y, size)
+
+    def draw(self):
+        return f"""
+({self.x}, {self.y})\n{self.size}
+         , - ~ ~ ~ - ,
+     , '               ' ,
+   ,                       ,
+  ,                         ,
+ ,                           ,
+ ,                           ,
+ ,                           ,
+  ,                         ,
+   ,                       ,
+     ,                  , '
+       ' - , _ _ _ ,  '
+"""
+
+
+def main():
+    c = Circle(1, 2, 3)
+    print(c.shape())
+    print(c.draw())
+
+main()
