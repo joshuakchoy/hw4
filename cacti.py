b2d6d102f9f2915d31f0f320db450d657c67f0fd
Joshua Choy <joshkchoy@gmail.com>
2025-10-14 23:08:56 -0700

    Fixed cacti.py

--- cacti.py
+++ cacti.py
@@ -1,24 +1,18 @@
-def cacti_number(func):
-    def wrapper(plot):
-
-        rows = len(plot)
-        cols = len(plot[0])
-        counter = 0
-
-        temp = [row[:] for row in plot]
-
-        for i in range(rows):
-            for j in range(cols):
-                if temp[i][j] == 0:
-                    up = i == 0 or temp[i - 1][j] == 0
-                    down = i == rows - 1 or temp[i + 1][j] == 0
-                    left = j == 0 or temp[i][j - 1] == 0
-                    right = j == cols - 1 or temp[i][j + 1] == 0
-
-                    if up and down and left and right:
-                        temp[i][j] = 1
-                        counter += 1
-
-        func(plot)
-        return counter
-    return wrapper
+def cacti_number(plot: list) -> int:
+    if not isinstance(plot, list):
+        raise TypeError
+    counter = 0
+    col = len(plot)
+    for y in range(0, col): 
+        row = len(plot[y])
+        for x in range(0, row): 
+            if plot[y][x] == 0: 
+                # check adjacent slots
+                empty = x - 1 < 0 or plot[y][x - 1] == 0
+                empty = empty and (x + 1 >= row or plot[y][x + 1] == 0)
+                empty = empty and (y - 1 < 0 or plot[y - 1][x] == 0)
+                empty = empty and (y + 1 >= col or plot[y + 1][x] == 0)
+                if empty:
+                    counter += 1
+                    plot[y][x] = 1
+    return counter
